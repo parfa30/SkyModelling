4b9e11a17b1c04b584a099023ceefc187fcceadf
Parker Fagrelius <parkerf@Parkers-MacBook-Pro.local>
2017-05-05 14:13:29 -0700

    fit spectra

--- fit_spectra.py
+++ fit_spectra.py
@@ -28,74 +28,82 @@ from scipy.linalg import lstsq
 from astropy.io import fits
 from scipy import interpolate
 import multiprocessing
+from scipy.special import eval_legendre, eval_chebys
+from numpy.linalg import inv
+import statsmodels.api as sm
+from datetime import datetime
 
 parallel=True
 MPI=False
 
 #Directory to save data
-SAVE_DIR = os.getcwd()+'/split_spectra/'
+SAVE_DIR = os.getcwd()+'/split_flux/'
 #Directory where all SpFrame flux files reside
-SPECTRA_DIR = os.getcwd()+'/sky_flux/'
-
-#Files from UVES that contain all airglow lines downloaded 
-AIRGLOW_DIR = '/Users/parkerf/Research/BOSS_Sky/flux_repo/SkyModelling/AirglowSpectra/'
+SPECTRA_DIR = os.getcwd()+'/sigma_sky_flux/'
 
 def main():
     # Load spectra data
-    spectra_files = glob.glob(SPECTRA_DIR+'*.npy')
-    print('Will be analyzing %d plate files' %len(spectra_files))
+    SPECTRA_FILES = os.listdir(SPECTRA_DIR)
+    
+    #Compare with what has already been done
+    COMPLETE_FILES = os.listdir(SAVE_DIR)
+    Complete_Plate_Names = []
+    All_Plate_Names = []
+    for d in COMPLETE_FILES:
+        Complete_Plate_Names.append(d[-23:-19])
+    for d in SPECTRA_FILES:
+        All_Plate_Names.append(d[-23:-19])
+
+    plates_needed_idx = [i for i, x in enumerate(All_Plate_Names) if x not in Complete_Plate_Names]
+    SPECTRA = []
+    for x in plates_needed_idx:
+        SPECTRA.append(SPECTRA_FILES[x])
+    print('Will be analyzing %d plate files' %len(SPECTRA))
 
     #Get meta data
     global MetaData
     MetaData = np.load('meta_rich.npy')
-    
-    #Get zodi
-    global ZodiLookup
-    ZodiLookup = np.load('zodi_spectra.npy')
+    print("got MetaData")
     
     #Get Airglowlines
-    AirglowFiles = glob.glob(AIRGLOW_DIR+'/*.dat')
+    AIRGLOW_DIR = '/Users/parkerf/Research/SkyModel/SkyModelling/AirglowSpectra/cosby'
+    AirglowFiles = glob.glob(AIRGLOW_DIR+'/*.txt')
+
+    global AirglowLines
     AirglowLines = []
     for file in AirglowFiles:
-        data = np.genfromtxt(file,skip_header=3,dtype=[('Sequence', '<i4'), ('CENTER', '<f8'), ('INT', '<f8'), ('FWHM', '<f8'), ('FLUX', '<f8')])
-        AirglowLines.append(np.array(data))
+        data = pd.read_csv(file,delim_whitespace=True)
+        d = data.to_records(index=False)
+        AirglowLines.append(np.array(d))
     AirglowLines = np.hstack(AirglowLines)
 
-    #Identify only "significant" lines and djust wavelength to vacuum 
-    global VacLines
-    sig = np.where(AirglowLines['FLUX']>1.)
-    VacLines = air_to_vac(AirglowLines['CENTER'])
-    VacLines = VacLines[sig]/10. #nm to A
-    print("Number of Airglow Lines we will fit for: ", len(VacLines))
+    
 
     ############
     ## SCRIPT ##
     ############
-
     
     if parallel:
         ## implement if MPI
         #multiprocessing speedup
         pool = multiprocessing.Pool(processes=32)
-        data = pool.map(fit_and_separate_spectra, spectra_files)
+        data = pool.map(fit_and_separate_spectra, SPECTRA)
         pool.terminate()
     else:
-        data = [fit_and_separate_spectra(p) for p in spectra_files]
+        data = [fit_and_separate_spectra(p) for p in SPECTRA]
 
+def get_vac_lines():
+    b_sig = np.where(AirglowLines['obs_eint']>10)
+    bVL = air_to_vac(AirglowLines['obs_wave'])
+    bVL = bVL[b_sig] #nm to A
+    blueVacLines = bVL[bVL<700]
 
-def get_zodi(ecl_lat, ecl_lon, wave_range):
-    """ This returns the zodiacal spectrum over a specified wavelength range for a 
-    given ecl_lat and ecl_lon. It takes the value of the zodiacal light at 500nm 
-    for the lookup table "ZodiLookup" and then multiplies by the relative brightness
-    at the given wavelength (in a solar spectrum). 
-    """
-    l = np.argmin([np.abs(ecl_lon-lon) for lon in ZodiLookup[0]])
-    b = np.argmin([np.abs(ecl_lat-lat) for lat in ZodiLookup[1]])
-    zodi = ZodiLookup[2][l][b]
+    r_sig = np.where(AirglowLines['obs_eint']>3)
+    rVL = air_to_vac(AirglowLines['obs_wave'])
+    rVL = rVL[r_sig] #nm to A
+    redVacLines = rVL[rVL>560]
 
-    flux = interpolate.interp1d(zodi['WAVE'],zodi['ZODI'])
-    Zodi = flux(wave_range)
-    return Zodi
+    return blueVacLines, redVacLines
 
 def clean_spectra(spectrum):
     """Takes out all nan/inf so lstsq will run smoothly
@@ -105,7 +113,9 @@ def clean_spectra(spectrum):
     wave = spectrum['WAVE'][ok]
     sky = spectrum['SKY'][ok]
     sigma = spectrum['SIGMA'][ok]
-    return [wave,sky,sigma]
+    disp = spectrum['DISP'][ok]
+    
+    return [wave,sky,sigma,disp]
 
 def air_to_vac(wave):
     """Index of refraction to go from wavelength in air to wavelength in vacuum
@@ -126,7 +136,7 @@ def air_to_vac(wave):
     vac_wave = nn*wave
     return vac_wave
 
-def airglow_line_components(airglow_lines, wave_range, sigma_range):
+def airglow_line_components(airglow_lines, wave_range, disp_range):
     """ Takes each Airglow line included in the analysis and creates a gaussian profile 
     of the line. 
     INPUT: - List of airglow lines wanted to model
@@ -139,68 +149,75 @@ def airglow_line_components(airglow_lines, wave_range, sigma_range):
     for line in airglow_lines:
         ss = []
         for i, w in enumerate(wave_range):
-            sig = sigma_range[i]
+            sig = disp_range[i]
             ss.append(np.exp(-0.5*((w-line)/sig)**2))
         AA.append(ss)
     return np.vstack(AA)
 
-def lin_regress_model(airglow_lines, ecl_lat, ecl_lon, wave_range, sigma_range, sky_spectra):
-    AA = airglow_line_components(airglow_lines, wave_range, sigma_range)
+def linear_model(spectrum, num_cont, airglow_lines):
+    wave_range, sky_spectra, sigma_range, disp_range = clean_spectra(spectrum)
+
+    AA = airglow_line_components(airglow_lines, wave_range, disp_range)
 
     # Continuum model
-    A0 = np.ones(len(wave_range))
-    A1 = wave_range
-    A2 = np.square(wave_range)
-    A3 = get_zodi(ecl_lat, ecl_lon, wave_range)
+    AC = []
+    for i in range(num_cont):
+        AC.append(eval_legendre(i, wave_range))
+    AC = np.array(AC)
+    A = np.stack(np.vstack((AC,AA)),axis=1)
 
-    A = np.stack(np.vstack((A0,A1,A2,A3,AA)),axis=1)
-    lsq = lstsq(A,sky_spectra)
-
-    num_comp = A.shape[1] - len(AA)
-
-    model = np.dot(A,lsq[0])
+    results = sm.OLS(sky_spectra, A).fit()
+    params = results.params
+    model = np.dot(A, params)
     
-    R_1 = np.sum([(i-np.mean(sky_spectra))**2 for i in model])
+    #R^2
+    resids = sky_spectra-model
+    R_1 = np.sum([(i)**2 for i in resids])
     R_2 = np.sum([(i-np.mean(sky_spectra))**2 for i in sky_spectra])  
-    R = R_1/R_2  
-    return [np.dot(A,lsq[0]),A,lsq,num_comp,R]
+    R = 1-(R_1/R_2)   
 
-def split_spectra(model,flux):
-    """ Takes model and separates the continuum from the sky lines
-    """
-    A = model[1]
-    lsq = model[2]
-    num_comp = model[3]
-
-    cont = np.dot(A[:,0:num_comp],lsq[0][0:num_comp])
-    lines = np.dot(A[:,num_comp:],lsq[0][num_comp:])
-    res = flux - model[0]
-
-    return [lines, cont, res]
+    #Separate
+    cont = np.dot(A[:,0:num_cont],params[0:num_cont])
+    lines = np.dot(A[:,num_cont:],params[num_cont:])
+    res = sky_spectra - model
+    
+    return [wave_range, lines, cont, res, R]
 
 def fit_and_separate_spectra(spectra_file):
+    BlueVacLines, RedVacLines = get_vac_lines()
+
     plate_num = spectra_file[-23:-19]
     print("Fitting spectra in plate %s" %plate_num)
-    spectra = np.load(spectra_file)
+    spectra = np.load(SPECTRA_DIR+spectra_file)
     this_plate = MetaData[MetaData['PLATE'] == int(plate_num)]
-    max_num = 50 #len(spectra) Number of spectra in a given plate that you want to run this for. Mostly for debugging
+    max_num = 10 #len(spectra) Number of spectra in a given plate that you want to run this for. Mostly for debugging
+    specnos = np.random.choice(this_plate['SPECNO'], size=max_num)
     num = 0
     data = []
    
-    for i, spectrum in enumerate(spectra):
+    for i, specno in enumerate(specnos):
         if num < max_num:
-            print('splitting spectra %d/%d for plate %s' % (i,len(spectra),plate_num))
-            this_obs = this_plate[this_plate['SPECNO'] == i]
-            wave,sky,sigma = clean_spectra(spectrum)
-            model = lin_regress_model(VacLines, this_obs['ECL_LAT'], this_obs['ECL_LON'], wave, sigma, sky)
-    
-            #Split model
-            split_model = split_spectra(model,sky)
-            model_fit = np.zeros(len(sky),dtype=[('WAVE','f8'),('LINES','f8'),('CONT','f8'),('RESIDS','f8'),('R','f8')])
-            model_fit['WAVE'] = wave
-            model_fit['LINES'] = split_model[0]
-            model_fit['CONT'] = split_model[1]
-            model_fit['RESIDS'] = split_model[2]
+            start = datetime.now()
+            print('splitting spectra %d/%d for plate %s' % (i,len(specnos),plate_num))
+            this_obs = this_plate[this_plate['SPECNO'] == specno]
+            if (this_obs['CAMERAS'] == b'b1') | (this_obs['CAMERAS'] == b'b2'):
+                model = linear_model(spectra[specno], 4, BlueVacLines)
+            elif (this_obs['CAMERAS'] == b'r1') | (this_obs['CAMERAS'] == b'r2'):
+                model = linear_model(spectra[specno], 3, RedVacLines)
+            else:
+                print("Don't recognize the camera")
+                model = [0,0,0,0,0]
+            fit_time = (datetime.now()-start).total_seconds()
+            #Save pieces
+            model_fit = np.zeros(len(model[0]),dtype=[('TIME','f8'),('PLATE','i4'),('COLOR','S2'),('NUM','i4'),('WAVE','f8'),('LINES','f8'),('CONT','f8'),('RESIDS','f8'),('R','f8')])
+            model_fit['TIME'] = fit_time
+            model_fit['PLATE'] = plate_num
+            model_fit['COLOR'] = this_obs['CAMERAS']
+            model_fit['NUM'] = specno
+            model_fit['WAVE'] = model[0]
+            model_fit['LINES'] = model[1]
+            model_fit['CONT'] = model[2]
+            model_fit['RESIDS'] = model[3]
             model_fit['R'] = model[4]
             data.append(model_fit)
             num+=1
