02073a8f7d4c982296f4e5b87fe5e6ad42526853
parfa30 <parfa30@users.noreply.github.com>
2018-06-27 09:51:06 -0700

    added more data cuts

--- make_mega_file.py
+++ make_mega_file.py
@@ -8,49 +8,79 @@ import numpy as np
 from astropy.io import fits
 import astropy.table
 
-file_dir = '/global/cscratch1/sd/parkerf/sky_flux/rich_mean_more/'
+def main():
 
-files = glob.glob(file_dir+'/*.fits')
-("got %d files from %s" % (len(files), file_dir))
+    file_dir = '/global/cscratch1/sd/parkerf/sky_flux/rich_mean/'
 
-Mega_file = []
-for filen in files:
-	d = astropy.table.Table.read(filen)
-	Mega_file.append(d)
-print("Got all the data")
+    files = glob.glob(file_dir+'/*.fits')
+    ("got %d files from %s" % (len(files), file_dir))
 
-MF = astropy.table.vstack(Mega_file)
-print("Made it into one big file")
+    Mega_file = []
+    for filen in files:
+    	d = astropy.table.Table.read(filen)
+    	Mega_file.append(d)
+    print("Got all the data")
 
-# Now get platequality info
-pl = astropy.table.Table.read(os.getcwd()+'/util/platelist.fits')
-Pl = pl[['PLATE','MJD','PLATEQUALITY','QUALCOMMENTS']]
-bad_plates = np.unique(Pl[Pl['PLATEQUALITY'] == 'bad ']['PLATE'])
-all_bad = []
-for plate in bad_plates:
-    this_data = Pl[Pl['PLATE'] == plate]
-    if len(np.unique(this_data['PLATEQUALITY'])) == 1:
-        all_bad.append(plate)
+    MF = astropy.table.vstack(Mega_file)
+    print("Made it into one big file")
 
-for plate in all_bad:
-    MF.remove_rows(MF['PLATE'] == plate)
+    good_MF = apply_platequality(MF)
+    good_MF.write('spframe_line_good.fits',format='fits')
 
-MF.write('spframe_line_mostly_good.fits',format='fits')
+    clean_good_MF = data_cuts(good_MF)
+    good_MF.write('good_data_%s.fits'% date.strftime('%y%m%d'),format='fits')
 
-some_bad = []
-for plate in bad_plates:
-    this_data = Pl[Pl['PLATE'] == plate]
-    if len(np.unique(this_data['PLATEQUALITY'])) == 2:
-        bad_ones = this_data[this_data['PLATEQUALITY'] == 'bad ']
-        for bad_one in bad_ones:
-            some_bad.append([bad_one['PLATE'],bad_one['MJD']])
-np.save('some_bad_list.npy', some_bad)
+def apply_platequality(DataTable):
+    # Now get platequality info
+    pl = astropy.table.Table.read(os.getcwd()+'/util/platelist.fits')
+    Pl = pl[['PLATE','MJD','PLATEQUALITY','QUALCOMMENTS']]
+    bad_plates = np.unique(Pl[Pl['PLATEQUALITY'] == 'bad ']['PLATE'])
 
-for bad_one in some_bad:
-    try:
-        MF.remove_rows((MF['PLATE'] == plate)&(MF['MJD'] == bad_one['MJD']))
-    except:
-        print(bad_one)
-MF.write('spframe_line_good.fits',format='fits')
-        
+    bad_days = []
+    for plate in bad_plates:
+        this_data = Pl[(Pl['PLATE'] == plate)&(Pl['PLATEQUALITY'] == 'bad')]
+        for bad_one in this_data:
+            bad_days.append([bad_one['PLATE'],bad_one['MJD']])
+                
+    for day in bad_days:
+        try:
+            DataTable.remove_rows((MF['PLATE'] == plate)&(MF['MJD'] == bad_one['MJD']))
+        except:
+            print(bad_one)
+
+    return DataTable
+
+def data_cuts(DataTable):
+    #Taken from the MakeTestDataFile.ipynb
+    remove_these_plates = [5745, 6138, 7258] 
+    #5745 because sky fiber test plate
+    #6138 because for no reason given, there isn't data for r2 in all observations and some done't have r1
+    #7258 "Forced complete"
+
+    print('outliers in blue')
+    out1 = DataTable[(DataTable['CAMERAS'] == 'b1')&(DataTable['std_cont_b_460'] > DataTable['mean_cont_b_460'])]['PLATE','IMG','FIB']
+    out2 = DataTable[(DataTable['CAMERAS'] == 'b1')&(DataTable['cont_b_460'] <= 0)]['PLATE','IMG','FIB']
+
+    print('outliers in red')
+    out3 = DataTable[(DataTable['CAMERAS'] == 'r1')&((DataTable['std_cont_r_720'] > DataTable['mean_cont_r_720']))]['PLATE','IMG','FIB']
+    out4 = DataTable[(DataTable['CAMERAS'] == 'r1')&(DataTable['cont_r_720'] < 0)]['PLATE','IMG','FIB']
+    out5 = DataTable[(DataTable['CAMERAS'] == 'r1')&(DataTable['cont_r_720'] == 0)]['PLATE','IMG','FIB']
+
+    print('stacked outliers')
+    outs = astropy.table.vstack[out1,out2,out3,out4,out5]
+
+    idx = []
+    for out in outs:
+        id0 = np.where((DataTable['IMG'] == out["IMG"])&(DataTable['FIB'] == out['FIB']))
+        idx.append(id0)
+    print('done idx')
+    idx = np.hstack(idx)
+    DataTable.remove_rows(idx[0])
+    print('done removing rows')
+
+    return DataTable
+
 print('finished writing!')
+
+if __name__ == '__main__':
+    main()
